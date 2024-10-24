ffc53f68660b1e9c9b181bfbb398fee4955daeb6
Akin Akinbobola <akin.akinbobola@nomba.com>
2024-10-24 09:07:19 -0300

    Initial commit

---
+++ src/main/java/dev/akinbobobla/dailybot/DailybotApplication.java
@@ -0,0 +1,13 @@
+package dev.akinbobobla.dailybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailybotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailybotApplication.class, args);
+    }
+
+}
